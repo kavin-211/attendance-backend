4365143111e65f388f0f3c9742a21728e15a8dd9
kavin-211 <kavinprasathvgp@gmail.com>
2025-12-23 21:32:04 +0530

    serverless code2

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
-// Vercel-compatible serverless function
-module.exports = (req, res) => {
+// Import the http module
+const http = require('http');
+
+// Create the server
+const server = http.createServer((req, res) => {
   // Set response header
   res.writeHead(200, { 'Content-Type': 'text/plain' });
   
   // Send response
   res.end('Hello, World!\n');
-};
+});
+
+// Define the port
+const PORT = 3000;
+
+// Start the server
+server.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}/`);
+});
